97c4e7059f3c4b4257aefb95a57e84df053cb883
L. Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-04-07 19:04:33 +0300

    Add files via upload

---
+++ Week2/Functii.py
@@ -0,0 +1,17 @@
+# print("Message on console")
+# variabila = 1
+# print("Message no {}".format(variabila))
+# raspuns_utilizator = input("Enter your name:")
+# print(raspuns_utilizator)
+
+# def my_function(param_1):
+#     pass
+
+def suma(a, b=6):
+    if type(a) == str:
+        return a
+    return a + b
+
+
+my_sum = suma(1, 4)
+print(my_sum)
