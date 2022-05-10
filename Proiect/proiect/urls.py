0d6d0116909ac1ff30e7e9a60b2ba6ffbe57c78c
Lucaciu Daiana-Larisa <78080241+daia-code@users.noreply.github.com>
2022-05-10 15:07:07 +0300

    Add files via upload

---
+++ Proiect/proiect/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.views import LoginView
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls'), {'next_page': '/'}, name='login'),
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('locations/', include('aplicatie1.urls')),
+    path('jobs/', include('aplicatieJobs.urls')),
+
+]
